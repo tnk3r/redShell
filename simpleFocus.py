1a0e4f88eee3e7caf0bcc38fc3b330ff95a0bd5f
tnk3r <hackth3planet>
2016-11-17 19:37:08 -0800

    cleaned simple focus

--- simpleFocus.py
+++ simpleFocus.py
@@ -4,14 +4,17 @@ print "Connecting to :"+str(address)
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 sock.connect((str(address), 1111))
 
+def floop():
+    for i in range(50, 66):
+        if i != "":
+            sock.sendall("#$EXT:S:HCFOCUS:"+str(int(i)*1000)+":\n")
+            time.sleep(0.25)
+
 while True:
     print "Enter a number between (0-655) to set Focus Encoder Position."
     value = raw_input("Set Encoder Value:  ")
     if value == "loop":
-        for i in range(50, 65):
-            if i != "":
-                sock.sendall("#$EXT:S:HCFOCUS:"+str(int(i)*1000)+":\n")
-                time.sleep(0.25)
+        floop()
     if value != "Q":
         sock.sendall("#$EXT:S:HCFOCUS:"+str(int(value)*100)+":\n")
     else:
